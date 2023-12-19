e8db14db428967fef0270dcea8d16853033bebda
Vlad <vladsfx@gmail.com>
2023-12-19 12:39:04 +0300

    Динамическая передача свойств компонента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        { name: 'Jhon Smith', salary: 1000, increase: true },
+        { name: 'Alex Bradly', salary: 800, increase: false },
+        { name: 'Ann Thomson', salary: 900, increase: false },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -15,7 +22,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
