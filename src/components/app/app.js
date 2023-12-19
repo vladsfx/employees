088b0c431f0b3278362b04d2d5a16aa2743b9a8b
Vlad <vladsfx@gmail.com>
2023-12-19 13:57:16 +0300

    Работа со списками.Атрибут key

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,9 @@ import './app.css';
 function App() {
 
     const data = [
-        { name: 'Jhon Smith', salary: 1000, increase: true },
-        { name: 'Alex Bradly', salary: 800, increase: false },
-        { name: 'Ann Thomson', salary: 900, increase: false },
+        { name: 'Jhon Smith', salary: 1000, increase: false, id: 1 },
+        { name: 'Alex Bradly', salary: 800, increase: true, id: 2 },
+        { name: 'Ann Thomson', salary: 900, increase: false, id: 3 },
     ];
 
     return (
